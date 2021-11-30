530920aa509f6443ab322a9dee94cffafb3ea497
Harysson <haryssonsoares@gmail.com>
2021-11-30 19:52:40 -0300

    wip: signup page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Card, Form } from "react-bootstrap";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
+
+const LoginForm: React.FC = () => {
+  return (
+    <>
+      <Form.Group>
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Digite seu email" />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Senha</Form.Label>
+        <Form.Control type="password" />
+      </Form.Group>
+    </>
+  );
+};
+
+const Login: React.FC = () => {
+  const methods = useFormContext();
+
+  return (
+    <div className="d-flex justify-content-center align-items-center h-100vh bg-dark">
+      <Card>
+        <Card.Header className="text-center">Login</Card.Header>
+        <Card.Body>
+          <FormProvider {...methods}>
+            <Form>
+              <LoginForm />
+            </Form>
+          </FormProvider>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
