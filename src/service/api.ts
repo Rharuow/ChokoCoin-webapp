7558f1236b5ab26e73e3bfbe4d33248730464288
Harysson <haryssonsoares@gmail.com>
2021-12-02 17:16:04 -0300

    feat: service api file

---
+++ src/service/api.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import qs from "qs";
+import { getSession } from "next-auth/client";
+
+axios.defaults.paramsSerializer = (params) =>
+  qs.stringify(params, { arrayFormat: "brackets" });
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASEPATH,
+  timeout: 1000,
+  headers: { "Content-Type": "application/json" },
+});
+
+const awakeServer = api
+  .get("/")
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+api.interceptors.request.use(async (config) => {
+  const session = await getSession();
+  if (session?.token !== undefined)
+    config = { headers: { Authorization: `Bearer ${session.token}` } };
+  return config;
+});
+
+const nextAuth = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+  // headers: { Origin: `${process.env.NEXT_PUBLIC_SITE_URL}` },
+});
+
+export { api, awakeServer, nextAuth };
