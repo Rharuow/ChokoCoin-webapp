2b9c55bc99a148f6c596984b3321962d162e419c
Harysson <haryssonsoares@gmail.com>
2021-12-13 16:59:42 -0300

    feat: signout button

--- src/domain/Home/index.tsx
+++ src/domain/Home/index.tsx
@@ -1,48 +1,60 @@
-import React, { useContext } from 'react'
-import { Button, Card } from 'react-bootstrap'
+import { signOut } from "next-auth/client";
+import React, { useContext } from "react";
+import { Button, Card } from "react-bootstrap";
 
-import { HomeContext } from '../../pages/home'
-import ModalFormProject from './Modal'
+import { HomeContext } from "../../pages/home";
+import ModalFormProject from "./Modal";
 
 const Content: React.FC = () => {
-    const {user, projects, modalIsOpen, setIsOpen} = useContext(HomeContext)
+  const { user, projects, modalIsOpen, setIsOpen } = useContext(HomeContext);
 
-    return (
-      <div className="h-100vh-min bg-dark">
-        <ModalFormProject />
-        <div className="flex-center-x-y flex-wrap flex-column h-100vh">
-          <Card>
-            <Card.Header>
-              <h1 className="text-center">Bem vindo {user?.username}</h1>
-            </Card.Header>
-            <Card.Body>
-              <h2>Projetos:</h2>
-              {projects &&
-                projects.map((project) => (
-                  <Card key={project.name}>
-                    <Card.Body>
-                      <p>nome: {project.name}</p>
-                      <p>valor: {project.value}</p>
-                      {
-                        project && project?.partners && project?.partners.length > 0 && project.partners.map((partner, index) => (
-                          <div key={index}>
-                            <p>Participantes</p>
-                              <li>Nome: {partner.username}</li>
-                              <li>Valor: {partner.value}</li>
-                          </div>
-                        ))
-                      }
-                    </Card.Body>
-                  </Card>
-                ))
-              }
-            </Card.Body>
-          </Card>
-          
-          <Button onClick={() => setIsOpen(!modalIsOpen)} className="mt-5 rounded-circle">+</Button>
-        </div>
+  return (
+    <div className="h-100vh-min bg-dark">
+      <ModalFormProject />
+      <div className="flex-center-x-y flex-wrap flex-column h-100vh">
+        <Button
+          variant="danger"
+          className="position-absolute z-1 top-0 end-0"
+          onClick={() => signOut()}
+        >
+          Sair
+        </Button>
+        <Card>
+          <Card.Header>
+            <h1 className="text-center">Bem vindo {user?.username}</h1>
+          </Card.Header>
+          <Card.Body>
+            <h2>{projects?.length > 0 ? "Projetos:" : "Sem projetos"}</h2>
+            {projects?.length > 0 &&
+              projects.map((project) => (
+                <Card key={project.name}>
+                  <Card.Body>
+                    <p>nome: {project.name}</p>
+                    <p>valor: {project.value}</p>
+                    {project &&
+                      project?.partners &&
+                      project?.partners.length > 0 &&
+                      project.partners.map((partner, index) => (
+                        <div key={index}>
+                          <p>Participantes</p>
+                          <li>Nome: {partner.username}</li>
+                          <li>Valor: {partner.value}</li>
+                        </div>
+                      ))}
+                  </Card.Body>
+                </Card>
+              ))}
+          </Card.Body>
+        </Card>
+
+        {user?.is_admin && (
+          <Button onClick={() => setIsOpen(!modalIsOpen)} className="mt-5">
+            Cadastrar de projeto
+          </Button>
+        )}
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Content
+export default Content;
